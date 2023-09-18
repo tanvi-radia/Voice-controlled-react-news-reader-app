5b3118d014feaa9c7f2f9770b9d601e87a784f0e
Tanvi Radia <139433835+tanvi-radia@users.noreply.github.com>
2023-09-18 22:19:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
 
   useEffect(() => {
     alanBtn({
-      key: '64370f4c903e66c5b517887fefa45c1b2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      key: '72a57b9facb4e433249a86f66f07839b2e956eca572e1d8b807a3e2338fdd0dc/stage',
       onCommand: ({ command, articles, number }) => {
         if (command === 'newHeadlines') {
           setNewsArticles(articles);
